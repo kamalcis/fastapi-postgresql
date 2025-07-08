80506553cf90fc24b3104587021bb9819bc24acb
Abu hena mostafa Kamal <kamalcis@gmail.com>
2025-07-08 13:25:53 +0600

    Docker Implemented

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Build Docker image to Local Docker Desktop
+        CMD> docker build -t fastapi-app 
+# Run the docker container from your local
+        CMD> docker run -d -p 8000:80 fastapi-app
+
+
+The docker image name is fastapi-app
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+class Blog(BaseModel):
+    title: str
+    description: str
+    published: Optional[bool]
+
+@app.post('/blog/create')
+def createBlog(blog: Blog):
+    return blog
+
 
 @app.get('/')
 def index():
